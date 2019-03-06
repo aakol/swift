a5acff6efe24c218ee9ad47f54dd8fc7f1e63c49
aakol <akol@poczta.onet.pl>
2019-03-06 09:40:50 -0800

    Initial commit

---
+++ widoki/widoki/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  widoki
+//
+//  Created by tech on 21/02/2019.
+//  Copyright © 2019 tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
